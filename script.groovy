c2fc359960b86444c147ab72fd97bd1c79451dc8
Ans Saeed <123291198+Ans-Saeed@users.noreply.github.com>
2023-05-03 16:49:40 +0500

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,42 @@
+def runAPP(){
+   sh 'echo Running the application...'
+     sh 'docker-compose down && docker-compose up -d'
+}
+def buildImage(){
+   withCredentials([
+        usernamePassword(credentialsId:'dockerhub-credentials', usernameVariable: 'USER', passwordVariable: 'PASS')
+    ]){
+            echo "building the docker image of application..."
+            sh 'docker build -t anssaeed/my-repo:${IMAGR_NAME} .'
+            sh 'echo $PASS | docker login -u $USER --password-stdin'
+            sh 'docker push anssaeed/my-repo:${IMAGR_NAME}'
+   }
+
+}
+
+def incrementVersion(){
+        sh 'npm --no-git-tag-version version patch'
+                   def matcher = readFile('package.json.xml') =~ '<version>(.+)</version>'
+                   def version =matcher[0][1]
+                   env.IMAGE_NAME="$version-$BUILD_NUMBER"
+}
+
+def commitVersion(){
+      withCredentials([
+        usernamePassword(credentialsId:'git-hub-access-key', usernameVariable: 'USER', passwordVariable: 'PASS')
+    ]){
+
+                sh 'echo Minor Version Bump...'
+                sh 'git status'
+                sh 'git config user.email jenkins@example.com'
+                sh 'git config user.name jenkins'
+                    sh "git remote set-url origin https://${PASS}@github.com/${USER}/node-todo-cicd.git"
+                sh 'git add .'
+                sh 'git commit -m "[ci skip] : version bump"'
+                sh 'git push origin HEAD:master'
+
+            }        
+}
+
+
+return this
